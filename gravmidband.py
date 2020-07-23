Fix units of beta

gravmidband.py
@@ -841,10 +841,11 @@ class PhaseTransition:
     def OmegaGW(self, f, Ts, alpha=1):
         """Total OmegaGW: this is just sound wave dropping the subdominant bubbles and turbulence following 1910.13125.
         We pick alpha = 1 as a fiducial model. It can be from 0.5 to 4 ish.
+        beta is beta/H and can be 0.01 < b/H < 1
         Ts is in GeV and can have a variety of values."""
         #Eyeballing the right panel of Figure 17 of 1809.08242 gives alpha = (10 beta/H)^0.8
-        beta = alpha**(1/0.8) / 10. * self.Hubble(Ts)
         #Eq. 6 of 1910.13125
+        beta = alpha**(1/0.8) / 10.
         cRs = (8 * math.pi)**(1./3) / beta * np.max([self.vw, self.cs])
         return self.OmegaSW0(f, cRs, Ts, alpha) + self.OmegaTB0(f, cRs, Ts, alpha)
 