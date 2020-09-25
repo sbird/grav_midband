c25e36a27d918a3b55dd4fe09f2127d108ab719f
Simeon Bird <sbird@ucr.edu>
2020-09-25 10:12:38 -0700

    Add beta to likelihood plot

--- plot_likelihood.py
+++ plot_likelihood.py
@@ -13,7 +13,7 @@ def make_plot(chainfile, savefile, true_parameter_values=None, ranges=None, stri
     if string:
         pnames = [ r"G\mu", r"\mathrm{SMBBH}", r"\mathrm{IMRI}", r"\mathrm{EMRI}"]
     else:
-        pnames = [ r"T_\ast", r"\mathrm{SMBBH}", r"\mathrm{IMRI}", r"\mathrm{EMRI}", r"\alpha"]
+        pnames = [ r"T_\ast", r"\mathrm{SMBBH}", r"\mathrm{IMRI}", r"\mathrm{EMRI}", r"\alpha", r"\beta"]
     prange = None
     if ranges is not None:
         prange = {pnames[i] : ranges[i] for i in range(len(pnames))}
@@ -37,10 +37,10 @@ def make_plot(chainfile, savefile, true_parameter_values=None, ranges=None, stri
         ticks = {pnames[0]: [np.log(1e-20), np.log(1e-17), np.log(1e-15)]}
         ticklabels = {pnames[0] : [r"$10^{-20}$", r"$10^{-17}$", r"$10^{-15}$"]}
     else:
-        ticks = {pnames[0]: [np.log(1e2), np.log(1e4), np.log(1e6), np.log(1e9), np.log(1e11)],
-                 pnames[4]: [np.log(1e-4), np.log(1e-3), np.log(1e-2), np.log(0.1), 0]}
-        ticklabels = {pnames[0] : [r"$10^{2}$", r"$10^{4}$", r"$10^{6}$", r"$10^{9}$", r"$10^{11}$"],
-                      pnames[4]: [r"$10^{-4}$", r"$10^{-3}$", r"$0.01$", r"$0.1$", r"$1.0$"]}
+        ticks = {pnames[0]: [np.log(1e2), np.log(1e4), np.log(1e6), np.log(1e9)]}#, np.log(1e11)
+                 #pnames[4]: [np.log(1e-4), np.log(1e-3), np.log(1e-2), np.log(0.1), 0]}
+        ticklabels = {pnames[0] : [r"$10^{2}$", r"$10^{4}$", r"$10^{6}$", r"$10^{9}$", r"$10^{11}$"]}#,
+                      #pnames[4]: [r"$10^{-4}$", r"$10^{-3}$", r"$0.01$", r"$0.1$", r"$1.0$"]}
 
     for pi in range(samples.shape[1]):
         for pi2 in range(pi + 1):
@@ -59,29 +59,22 @@ def make_plot(chainfile, savefile, true_parameter_values=None, ranges=None, stri
             if pi2 < pi:
                 if not np.isnan(true_parameter_values[pi]):
                     ax.axhline(true_parameter_values[pi], color='gray', ls='--', lw=2)
-            #Plot alpha as log on the phase plot
-#     if not string:
-#         for pi in range(samples.shape[1]):
-#             ax = subplot_instance.subplots[4,pi]
-#             ax.set_yscale('log')
-#             if pi == 4:
-#                 ax.set_xscale('log')
     plt.savefig(savefile)
 
 if __name__ == "__main__":
     #For PT
-    true_vals = [np.nan, 56., 0.005, 1, np.nan]
+    true_vals = [np.nan, 56., 0.005, 1, np.nan, np.nan]
     #ranges
-    ranges = [[4.6, 20.8], [0, 100], [0,1], [0.1,10], [np.log(1e-10),0]]
-    make_plot("samples_ligo_lisa_phase_bbh.txt", "like_ligo_lisa_phase_bbh.pdf", true_parameter_values = true_vals, ranges=ranges, string=False)
-    make_plot("samples_ligo_lisa_tiango_phase_bbh.txt", "like_ligo_lisa_tiango_phase_bbh.pdf", true_parameter_values = true_vals, ranges=ranges, string=False)
-    make_plot("samples_ligo_lisa_decigo_phase_bbh.txt", "like_ligo_lisa_decigo_phase_bbh.pdf", true_parameter_values = true_vals, ranges=ranges, string=False)
+    ptranges = [[4.6, 20.8], [0, 100], [0,1], [0.1,10], [np.log(1e-10),0]]
+    make_plot("samples_ligo_lisa_phase_bbh.txt", "like_ligo_lisa_phase_bbh.pdf", true_parameter_values = true_vals, ranges=ptranges, string=False)
+    make_plot("samples_ligo_lisa_tiango_phase_bbh.txt", "like_ligo_lisa_tiango_phase_bbh.pdf", true_parameter_values = true_vals, ranges=ptranges, string=False)
+    make_plot("samples_ligo_lisa_decigo_phase_bbh.txt", "like_ligo_lisa_decigo_phase_bbh.pdf", true_parameter_values = true_vals, ranges=ptranges, string=False)
 
     #For strings
     true_vals = [np.nan, 56., 0.005, 1]
     #ranges
-    ranges = [[np.log(1e-20), np.log(2e-13)], [55.5, 56.5], [0.0048,0.0051], [0.985,1.015]]
-    make_plot("samples_ligo_lisa_string_bbh.txt", "like_ligo_lisa_string_bbh.pdf", true_parameter_values = true_vals, ranges=ranges)
-    ranges = [[np.log(1e-20), np.log(2e-13)], [55.7, 56.3], [0.00495,0.00505], [0.995,1.005]]
-    make_plot("samples_ligo_lisa_tiango_string_bbh.txt", "like_ligo_lisa_tiango_string_bbh.pdf", true_parameter_values = true_vals, ranges=ranges)
-    make_plot("samples_ligo_lisa_decigo_string_bbh.txt", "like_ligo_lisa_decigo_string_bbh.pdf", true_parameter_values = true_vals, ranges=ranges)
+    srranges = [[np.log(1e-20), np.log(2e-13)], [55.5, 56.5], [0.0048,0.0051], [0.985,1.015]]
+    make_plot("samples_ligo_lisa_string_bbh.txt", "like_ligo_lisa_string_bbh.pdf", true_parameter_values = true_vals, ranges=srranges)
+    srranges = [[np.log(1e-20), np.log(2e-13)], [55.7, 56.3], [0.00495,0.00505], [0.995,1.005]]
+    make_plot("samples_ligo_lisa_tiango_string_bbh.txt", "like_ligo_lisa_tiango_string_bbh.pdf", true_parameter_values = true_vals, ranges=srranges)
+    make_plot("samples_ligo_lisa_decigo_string_bbh.txt", "like_ligo_lisa_decigo_string_bbh.pdf", true_parameter_values = true_vals, ranges=srranges)
